b61bf6cc77870f79a9f2021db9a0ed1b9694bee8
Araf-Khatri <arafkhatri0@gmail.com>
2023-09-09 00:50:07 +0530

    skill section changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const PersonalData = {
+const personalData = {
   name: "ARAF IBRAHIM KHATRI",
   profession: "Full Stack Developer",
   email: "arafkhatri0@gmail.com",
@@ -9,23 +9,43 @@ const PersonalData = {
     { name: "LinkedIn", link: "" },
     { name: "Github", link: "https://github.com/Araf-Khatri", isLast: true },
   ],
+  skills: [
+    "Python",
+    "Javascript",
+    "Typescript",
+    "MongoDB",
+    "SQL",
+    "Express",
+    "React / Redux",
+    "NodeJS",
+    "HTML",
+    "CSS",
+    "C",
+    "Firebase",
+    "REST API",
+    "Data Structures & Algorithms",
+    "Bootstrap",
+    " TailwindCSS",
+  ],
 };
 
 const contactsAndLinks = document.getElementById("personal-data");
+const skillsDataSection = document.getElementById("skills-data");
+
 let contactsEl = `
 <div class="grid grid-cols-contact items-center gap-x-1">
   <i
     class="fa fa-phone-square text-primary-900"
     aria-hidden="true"
   ></i>
-  <p class="text-sm text-end">${PersonalData.phoneNo}</p>
+  <p class="text-sm text-end">${personalData.phoneNo}</p>
   <i class="fa fa-envelope text-primary-900" aria-hidden="true"></i>
-  <p class="text-sm">${PersonalData.email}</p>
+  <p class="text-sm">${personalData.email}</p>
 </div>
 <div class="flex items-center gap-2">
 `;
 
-const socialLinks = PersonalData.socials;
+const socialLinks = personalData.socials;
 for (let { name, link, isLast } of socialLinks) {
   contactsEl += `<a href="${
     link || ""
@@ -33,4 +53,15 @@ for (let { name, link, isLast } of socialLinks) {
                   ${!isLast ? "<p>|</p>" : "</div>"}`;
 }
 
+const skills = personalData.skills;
+let skillsHTML = "";
+for (let skill of skills) {
+  skillsHTML += `
+  <div class="rounded-md">
+  <p class="text-base font">- ${skill}</p>
+  </div>\n
+  `;
+}
+
 contactsAndLinks.insertAdjacentHTML("beforeend", contactsEl);
+skillsDataSection.insertAdjacentHTML("afterbegin", skillsHTML);
